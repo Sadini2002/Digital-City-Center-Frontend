d358b4e6a7796b9edcbd873426f202674fcf2830
Sadini <jasingha2002sadninipunika@gamil.com>
2026-05-27 12:48:54 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom'
+import './index.css'
+
+
+function App() {
+  return (
+    <>
+    <h1 className='bg-black text-center text-white'> HI</h1>
+     
+   
+      
+    </>
+  )
+}
+
+export default App
