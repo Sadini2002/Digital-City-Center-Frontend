ef846365cc5d103d2b0e01eaa10f55f3091d6d61
Tharindi2004 <tharindi.20240581@iit.ac.lk>
2026-06-25 12:30:48 +0530

    Update API endpoints in shopService to include 'url' prefix for shop and product retrieval

--- src/services/shopService.js
+++ src/services/shopService.js
@@ -5,7 +5,7 @@ const API = "http://localhost:5000/api/v1/shops";
 export const getAllShops = () => axios.get(API);
 
 export const getShop = (shopUrl) =>
-  axios.get(`${API}/${shopUrl}`);
+  axios.get(`${API}/url/${shopUrl}`);
 
 export const getShopProducts = (shopUrl) =>
-  axios.get(`${API}/${shopUrl}/products`);
+  axios.get(`${API}/url/${shopUrl}/products`);
